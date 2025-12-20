67e4e7084d9c7815a27a5f9af180d94f8627278c
Noor Mohammed <nmohame22@yahoo.com>
2025-12-20 15:35:02 -0500

    Password Manager API

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nmoha/go-project
 
 go 1.25.4
+
+require golang.org/x/crypto v0.46.0
--- main.go
+++ main.go
@@ -1,33 +1,82 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"net/http"
 	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 
-	http.HandleFunc("/signup", SignUp)
-	http.HandleFunc("/login", Login)
-	http.HandleFunc("/addpassword", AddPassword)
+	http.HandleFunc("/sign_up", SignUp)
+	http.HandleFunc("/log_in", Login)
+	http.HandleFunc("/add_password", AddPassword)
 	http.ListenAndServe(":8080", nil)
 }
 
-var users = map[string]string{}
-var userPasswords = map[string][]map[string]string{}
-var AddPasswords = map[string][]map[string]string{}
+type MessageResponse struct {
+	Message string `json:"message"`
+}
+
+type PasswordInfo struct {
+	Name     string `json:"name"`
+	Value    string `json:"value"`
+	Platform string `json:"platform"`
+}
+type User struct {
+	Username  string
+	Password  string
+	Passwords []PasswordInfo
+}
+
+var users = map[string]User{}
+var tokens = map[string]string{}
+
+// var userPasswords = map[string][]map[string]string{}
+//var AddPasswords = map[string][]map[string]string{}
+
+type SignUpRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AddPasswordRequest struct {
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Platform string `json:"platform"`
+}
+
+func generateToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return hex.EncodeToString(b)
+}
 
 func SignUp(
 	w http.ResponseWriter,
-	r *http.Request) {
+	r *http.Request,
+) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var body map[string]string
-	json.NewDecoder(r.Body).Decode(&body)
+	var body SignUpRequest
 
-	username := strings.TrimSpace(body["username"])
-	password := body["password"]
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error":"invalid input"}`))
+		return
+	}
+	username := body.Username
+	password := body.Password
 
 	if username == "" || password == "" {
 		w.WriteHeader(http.StatusBadRequest)
@@ -37,30 +86,37 @@ func SignUp(
 
 	if len(username) < 6 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error":"wrong uersname}`))
+		w.Write([]byte(`{"error":"username must be at least 6 characters long"}`))
 		return
 	}
-	if users[username] != "" {
+
+	if _, exists := users[username]; exists {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`{"error":"User already exists"}`))
 		return
 	}
 
+	//	if users[username] != "" {
+	//		w.WriteHeader(http.StatusBadRequest)
+	//		w.Write([]byte(`{"error":"User already exists"}`))
+	//		return
+	//	}
+
 	if len(password) < 12 {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error":"Weak password"}`))
+		w.Write([]byte(`{"error":"password must be at least 12 characters long"}`))
 		return
 	}
 
-	hasCaital := false
+	hasCapital := false
 	for _, ch := range password {
 		if ch >= 'A' && ch <= 'Z' {
-			hasCaital = true
+			hasCapital = true
 			break
 		}
 	}
 
-	if !hasCaital {
+	if !hasCapital {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`{"error":"must contain capital letter"}`))
 		return
@@ -100,21 +156,44 @@ func SignUp(
 		return
 	}
 
-	commonPasswords := []string{
-		"password", "123456", "654321", "abc12345", "1q2w3e4r5t", "09876", "qwertyu",
-	}
-	for _, common := range commonPasswords {
-		if password == common {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"error":"password is too common"}`))
-			return
-		}
+	hashedPassword, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error":"Error hashing password"}`))
+		return
 	}
 
-	users[username] = password
-	userPasswords[username] = []map[string]string{}
+	//	commonPasswords := []string{
+	//		"password1234", "123456789!@#", "#@!987654321", "abc123456789", "1q2w3e4r5t6y",
+	//	}
+
+	//	for _, common := range commonPasswords {
+	//		if password == common {
+	//			w.WriteHeader(http.StatusBadRequest)
+	//			w.Write([]byte(`{"error":"password is too common"}`))
+	//			return
+	//		}
+	//	}
+
+	//	users[username] = password
+	//	userPasswords[username] = []map[string]string{}
+	//	w.WriteHeader(http.StatusCreated)
+	//	w.Write([]byte(`{"message":"Registration successful"}`))
+	//}
+
+	users[username] = User{
+		Username:  username,
+		Password:  string(hashedPassword),
+		Passwords: []PasswordInfo{},
+	}
+
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message":"Registration successful"}`))
+	json.NewEncoder(w).Encode(MessageResponse{
+		Message: "Registration successful",
+	})
 }
 
 func Login(
@@ -122,27 +201,60 @@ func Login(
 	r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var body map[string]string
-	json.NewDecoder(r.Body).Decode(&body)
+	var body LoginRequest
 
-	username := body["username"]
-	password := body["password"]
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error":"invalid input"}`))
+		return
+	}
 
-	storedPassword, ok := users[username]
+	username := body.Username
+	password := body.Password
+
+	user, ok := users[username]
 	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"error":"User not found"}`))
-		return
-	}
-
-	if storedPassword != password {
 		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte(`{"error":"Wrong Password"}`))
+		json.NewEncoder(w).Encode(MessageResponse{
+			Message: "Invalid Username or Password",
+		})
 		return
 	}
 
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(user.Password),
+		[]byte(password),
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(MessageResponse{
+			Message: "Invalid Username or Password",
+		})
+		return
+	}
+
+	//	storedPassword, ok := users[username]
+	//	if !ok {
+	//		w.WriteHeader(http.StatusNotFound)
+	//		w.Write([]byte(`{"error":"User not found"}`))
+	//		return
+	//	}
+
+	//	if storedPassword != password {
+	//		w.WriteHeader(http.StatusUnauthorized)
+	//		w.Write([]byte(`{"error":"Wrong Password"}`))
+	//		return
+	//	}
+	//	json.NewEncoder(w).Encode(MessageResponse{
+	//		Message: "Login successful",
+	//	})
+	token := generateToken()
+	tokens[token] = username
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"Login Successful"}`))
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": token,
+	})
 }
 
 func AddPassword(
@@ -150,24 +262,64 @@ func AddPassword(
 	r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var body map[string]string
-	json.NewDecoder(r.Body).Decode(&body)
+	var body AddPasswordRequest
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error":"invalid input"}`))
+		return
+	}
 
-	username := body["username"]
+	token := r.Header.Get("Authorization")
+	username, ok := tokens[token]
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"error":"Invalid token"}`))
+		return
+	}
 
-	if _, ok := users[username]; !ok {
-		w.WriteHeader(http.StatusNotFound)
+	//username := body.Username
+
+	user, ok := users[username]
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte(`{"error":"User not found"}`))
 		return
 	}
 
-	pass := map[string]string{
-		"name":     body["name"],
-		"value":    body["password"],
-		"platform": body["platform"],
+	hashedValue, err := bcrypt.GenerateFromPassword(
+		[]byte(body.Password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error":"Error hashing password"}`))
+		return
 	}
 
-	userPasswords[username] = append(userPasswords[username], pass)
+	pass := PasswordInfo{
+		Name:     body.Name,
+		Value:    string(hashedValue),
+		Platform: body.Platform,
+	}
+
+	user.Passwords = append(user.Passwords, pass)
+	users[username] = user
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message":"Password Saved"}`))
-	//	fmt.Fprintln(w, "password saved")
 }
+
+//	if _, ok := users[username]; !ok {
+//		w.WriteHeader(http.StatusNotFound)
+//		w.Write([]byte(`{"error":"User not found"}`))
+//		return
+//	}
+
+//	pass := map[string]string{
+//		"name":     body.Name,
+//		"value":    body.Password,
+//		"platform": body.Platform,
+//	}
+
+//	userPasswords[username] = append(userPasswords[username], pass)
+//	w.Write([]byte(`{"message":"Password Saved"}`))
+//}
